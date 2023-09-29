5f55fc61dd842278d17e8e54bdfc3640de2048db
Yuliya_Lukashova <y.lukashova@tg.dunice.net>
2023-09-29 11:38:53 +0300

    add dark theme

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <script setup>
 import { useUserStore } from './store';
 import AppCard from './components/AppCard.vue';
+import { useDark, useToggle } from "@vueuse/core";
+
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
 
 const userStore = useUserStore();
 
@@ -18,18 +22,37 @@ window.addEventListener('scroll', () => {
 </script>
 
 <template>
+  <label class="container">
+    <input id="check" type="checkbox" @click="toggleDark()">
+    <span></span>
+    <label for="check" class="shadow"></label>
+  </label>
   <h3 class="app-title">Endless list app</h3>
   <ul class="cards">
     <AppCard 
       v-for="user in userStore.users"
       :key="user.id"
       :user="user"
+      :users="userStore.users"
     />
   </ul> 
 </template>
 
 <style lang="scss">
 $font: 'Kanit', sans-serif;
+
+.dark {
+  .app-title{
+    color:rgb(255, 255, 255);
+  }
+  body {
+  background-color: #202020;
+}
+.card {
+  background-color: rgba(105, 105, 103, 0.58);
+  color: rgb(255, 255, 255);
+}
+}
 
 .app-title {
   text-align: center;
@@ -39,5 +62,60 @@ $font: 'Kanit', sans-serif;
 display: flex;
 align-items: center;
 flex-direction: column;
+}
+
+input[type = "checkbox"] {
+  visibility: hidden;
+  display: none;
+  appearance: none;
+  -webkit-appearance: none;
+}
+
+label.container {
+  margin-top: 15px;
+  position: absolute;
+  left: 46%;
+  display: block;
+  width: 60px;
+  height: 30px;
+  background: #d3d3d3;
+  border-radius: 50px;
+  position: relative;
+  cursor: pointer;
+  box-shadow: 9px 9px 29px #969696,
+             -9px -9px 29px #ffffff;
+}
+
+label.shadow {
+  width: 50px;
+  height: 20px;
+  position: absolute;
+  border-radius: 50px;
+  left: 5px;
+  top: 5px;
+  box-shadow: inset 3px 3px 10px #969696,
+              inset -3px -3px 5px #ffffffbd;
+  cursor: pointer;
+}
+
+.container span {
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  background-color: #6c8b3f;
+  margin: 5px;
+  border-radius: 50px;
+  font-size: 20px;
+  transition: all 0.3s ease-in;
+  position: relative;
+  left: 0;
+  z-index: 1;
+}
+
+input[type = "checkbox"]:checked ~ span {
+  left: 29px;
+  transition: 0.3s ease-in;
+  background: #0a100d;
+  transform: rotate(220deg);
 }
 </style>
--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,42 +1,46 @@
 <script setup>
 import { RecycleScroller } from 'vue-virtual-scroller';
-import 'vue-virtual-scroller/dist/vue-virtual-scroller.css'
+import 'vue-virtual-scroller/dist/vue-virtual-scroller.css';
 import { useUserStore } from '../store';
 
 const userStore = useUserStore();
 
 // userStore.users.forEach((user) => console.log(user));
 const props = defineProps({
+  users: {
+    type: Array,
+  },
   user: {
     type: Object,
     required: true,
     default: () => {},
   }
-})
+});
+console.log(props.users);
 
 </script>
 
 <template>
-  <RecycleScroller
+  <!-- <RecycleScroller
     class="scroller"
-    :items="userStore.users"
-    :item-size="4"
+    :items="users"
+    :item-size="32"
     key-field="id"
     v-slot="{ user }"
-  >
+  > -->
     <li class="card">
       <div>
         <img class="card__img" src="https://i.pinimg.com/474x/27/01/f5/2701f51da94a8f339b2149ca5d15d2a5.jpg">
       </div>
       <div class="card__user-info user-info">
-        <p class="user-info__name"><b>name: </b>{{ user?.name }}</p>
+        <p class="user-info__name"><b>name: </b>{{ user?.firstname}}</p>
         <p class="user-info__phone"><span>phone: </span>{{ user?.phone }}</p>
         <p class="user-info__email"><span>email: </span>{{ user?.email }}</p>
         <p class="user-info__gender"><span>gender: </span>{{ user?.gender }}</p>
         <p class="user-info__website"><span>website: </span>{{ user?.website }}</p>
       </div>
     </li>
-  </RecycleScroller>
+  <!-- </RecycleScroller> -->
 </template>
 
 <style lang="scss">
@@ -46,19 +50,16 @@ $font: 'Kanit', sans-serif;
   height: 100%;
 }
 
-
 .card {
   box-sizing: border-box;
-  width: 500px;
-  height: 230px;
-  background: rgba(220, 220, 207, 0.58);
+  width: 385px;
+  height: 195px;
   box-shadow: 12px 17px 51px rgba(0, 0, 0, 0.32);
   border-radius: 17px;
   transition: all 0.5s;
   display: flex;
-  color: black;
   margin: 10px;
-  padding: 15px;
+  padding: 11px;
   &:hover {
     border: 3px solid #6c8b3f;
   transform: scale(1.05);
@@ -66,16 +67,17 @@ $font: 'Kanit', sans-serif;
 }
 
 .card__img {
-  width: 150px;
-  height: 200px;
+  width: 140px;
+  height: 170px;
   border-radius: 17px;
 }
 
 .user-info {
   padding: 5px;
-  margin-left: 10px;
+  margin-left: 7px;
   font-family: $font;
   p {
+    font-size: 14px;
     margin: 5px;
   }
   span {
